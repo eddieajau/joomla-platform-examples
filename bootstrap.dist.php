7713f83047c4c92fb868be8abe95dbea405bd65a
eddieajau <andrew.eddie@newlifeinit.com>
2011-09-20 18:38:26 +1000

    Move examples across from the main Joomla Platform repo.

---
+++ bootstrap.dist.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Bootstrap file for the Joomla! Platform
+ *
+ * Rename this file to bootstrap.php and change to suit your environment.
+ *
+ * @package    Joomla.Examples
+ * @copyright  Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+// Change the following path to suit where you have cloned or installed the Joomla Platform repository.
+// The default setting assumes you have the joomla platform and examples folder at the same level.
+
+require dirname(dirname(__FILE__)).'/joomla-platform/libraries/import.php';
