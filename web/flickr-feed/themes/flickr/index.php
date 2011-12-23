2038e52ca0c977bdd29850ac5ea4f67cd8e5574b
eddieajau <andrew.eddie@newlifeinit.com>
2011-12-23 17:19:00 +1000

    Added file header and code assist comment.

--- web/flickr-feed/themes/flickr/index.php
+++ web/flickr-feed/themes/flickr/index.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * @package    Joomla.Examples
+ * @copyright  Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/* @var $this JView */
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
